7116a8cb647cf1bb12899961cc2c8c45b76361d4
stevenqhxu <stevenqhxu@tencent.com>
2020-03-05 22:51:54 +0800

    feat: 支持更多的回调特性

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict'
 /** 
  * https://www.npmjs.com/package/vue-check-view
- * stevenqhxu 参考这个改造的，扩张性更好一些。
+ * stevenqhxu 参考这个改造的，扩展性更好一些。
  * 1.支持传参最小高度和最小宽度的
  * 2.支持宽度的判断
  * 3.支持更多的参数返回
+ * 4.支持的更多特定事件回调
  */
 function getPlugin() {
     const EventTypes = {
@@ -41,98 +42,94 @@ function getPlugin() {
             itemIndex = 0;
 
 
-        window.addEventListener('scroll', function(){
-          //console.log('scroll');
-          scrollThrottledHandler();
+        window.addEventListener('scroll', function () {
+            scrollThrottledHandler();
         });
-        window.addEventListener('resize',function(){
-          //console.log('resize');
-          scrollThrottledHandler();
+        window.addEventListener('resize', function () {
+            scrollThrottledHandler();
         });
         // window.addEventListener('touchmove',function(){
         //   //console.log('touchmove');
         //   scrollThrottledHandler();
         // });
 
-        function scrollHandler(e){
-        let viewportTop = window.pageYOffset, //当前滚动的上下的距离
-            viewportBottom = window.pageYOffset + window.document.documentElement.clientHeight,
-            viewportLeft = window.pageXOffset, //左右滚动的距离
-            viewportRight = window.pageXOffset + window.document.documentElement.clientWidth,
-            viewportHeight = window.document.documentElement.clientHeight, //页面可视化窗口高度
-            viewportWidth = window.document.documentElement.clientWidth, //页面可视化窗口宽度
-            documentHeight = window.document.documentElement.scrollHeight; //页面全文高度
-        //documentWidth = window.document.documentElement.scrollWidth, //页面宽度 暂时用不到
-        //scrollPercent = roundPercent(window.pageYOffset / (documentHeight - viewportHeight)); //滚动的长度占可滚动高度的占比
+        function scrollHandler(e) {
+            let viewportTop = window.pageYOffset, //当前滚动的上下的距离
+                viewportBottom = window.pageYOffset + window.document.documentElement.clientHeight,
+                viewportLeft = window.pageXOffset, //左右滚动的距离
+                viewportRight = window.pageXOffset + window.document.documentElement.clientWidth,
+                viewportHeight = window.document.documentElement.clientHeight, //页面可视化窗口高度
+                viewportWidth = window.document.documentElement.clientWidth, //页面可视化窗口宽度
+                documentHeight = window.document.documentElement.scrollHeight; //页面全文高度
+            //documentWidth = window.document.documentElement.scrollWidth, //页面宽度 暂时用不到
+            //scrollPercent = roundPercent(window.pageYOffset / (documentHeight - viewportHeight)); //滚动的长度占可滚动高度的占比
 
 
-        scrollValue = viewportTop - scrollValue; //当前滚动的值
-        function getInType(i) {
-            const element = i.element,
-                rect = element.getBoundingClientRect(),
-                elementTop = rect.top + viewportTop, //元素的顶部距离文档顶部的距离
-                elementBottom = elementTop + rect.height, //元素底部距离文档顶部的距离
-                elementLeft = rect.left + viewportLeft, //元素的左边距离文档的左边的距离
-                elementRight = elementLeft + rect.width; //元素的右边距离文档的左边的距离
-            let elementHeight = element.offsetHeight;
-            let elementWidth = element.offsetWidth;
-            //console.log(i);
-            let minShowHeight = i.minShowHeight || 0, //最小展示高度 表示要超过该高度才算展示。小于这个高端算不展示
-                minShowWidth = i.minShowWidth || 0; //最小展示宽度 表示要超过该宽度才算展示。小于这个高端算不展示
-            //如果指定的高度大于元素高度，则用元素的高度或者宽度，即表示元素完全暴露在屏幕内。
-            minShowHeight = minShowHeight > elementHeight ? elementHeight : minShowHeight;
-            minShowWidth = minShowWidth > elementWidth ? elementWidth : minShowWidth;
-            let isTopIn = false,
-                isBottomIn = false,
-                isLeftIn = false,
-                isRightIn = false;
-            //console.log(minShowHeight + 'minShowHeight');
-            if (rect.top >= 0) { //顶部视口还没有贴到当前元素
-                isTopIn = viewportHeight - rect.top >= minShowHeight;
-            } else { //已经有一部分消失在顶部了，剩余的是否足够算展示
-                isBottomIn = rect.top + elementHeight >= minShowHeight;
-            }
+            scrollValue = viewportTop - scrollValue; //当前滚动的值
+            function getInType(i) {
+                const element = i.element,
+                    rect = element.getBoundingClientRect(),
+                    elementTop = rect.top + viewportTop, //元素的顶部距离文档顶部的距离
+                    elementBottom = elementTop + rect.height, //元素底部距离文档顶部的距离
+                    elementLeft = rect.left + viewportLeft, //元素的左边距离文档的左边的距离
+                    elementRight = elementLeft + rect.width; //元素的右边距离文档的左边的距离
+                let elementHeight = element.offsetHeight;
+                let elementWidth = element.offsetWidth;
+                let minShowHeight = i.minShowHeight || 1, //最小展示高度 表示要超过该高度才算展示。小于这个高端算不展示
+                    minShowWidth = i.minShowWidth || 1; //最小展示宽度 表示要超过该宽度才算展示。小于这个高端算不展示
+                //如果指定的高度大于元素高度，则用元素的高度或者宽度，即表示元素完全暴露在屏幕内。
+                //isForceHeight 强制是否强制使用传入的高度，isForceWidth 强制是否强制使用传入的宽度。为true表示不再和元素的高度或者宽度求最小值了。主要解决一些元素因为是动态插入的，导致初始的elementHeight是0或者很小。
+                minShowHeight = minShowHeight > elementHeight && !i.isForceHeight ? elementHeight : minShowHeight;
+                minShowWidth = minShowWidth > elementWidth && !i.isForceWidth ? elementWidth : minShowWidth;
+                let isTopIn = false,
+                    isTopInOnePixMore,
+                    isBottomIn = false,
+                    isBottomInOnePixMore = false,
+                    isLeftIn = false,
+                    isLeftInOnePixMore = false,
+                    isRightIn = false,
+                    isRightInOnePixMore = false;
+                if (rect.top >= 0) { //顶部视口还没有贴到当前元素
+                    isTopIn = viewportHeight - rect.top >= minShowHeight;
+                    isTopInOnePixMore = viewportHeight - rect.top > 0; //表示已经进入了，且超过1像素。
+                } else { //已经有一部分消失在顶部了，剩余的是否足够算展示
+                    isBottomIn = rect.top + elementHeight >= minShowHeight;
+                    isBottomInOnePixMore = rect.top + elementHeight >= 0; //表示已经进入了，且超过1像素。
+                }
 
-            if (rect.left >= 0) {
-                isLeftIn = viewportWidth - rect.left >= minShowWidth;
-            } else {
-                isRightIn = elementWidth + rect.left >= minShowWidth;
-            }
+                if (rect.left >= 0) {
+                    isLeftIn = viewportWidth - rect.left >= minShowWidth;
+                    isLeftInOnePixMore = viewportWidth - rect.left > 0; //表示已经进入了，且超过1像素。
+                } else {
+                    isRightIn = elementWidth + rect.left >= minShowWidth;
+                    isRightInOnePixMore = elementWidth + rect.left > 0; //表示已经进入了，且超过1像素。
+                }
 
-            let isInView = (isTopIn || isBottomIn) && (isRightIn || isLeftIn);
-
-            // 头部进入view viewportBottom - elementTop 
-            // 底部进入view elementBottom - viewportTop
-            let heightPercentInView = isTopIn || isBottomIn ? ((isBottomIn ? elementBottom : viewportBottom) - (isTopIn ? elementTop : viewportTop)) / rect.height : 0,
-                widthPercentInView = isLeftIn || isRightIn ? ((isRightIn ? elementRight : viewportRight) - (isLeftIn ? elementLeft : viewportLeft)) / rect.width : 0,
-                isAbove = heightPercentInView === 0 && rect.top <= 0, //是否在屏幕中顶部
-                isBelow = heightPercentInView === 0 && rect.top > 0, //是否在屏幕底部
-                isLeft = widthPercentInView === 0 && rect.left <= 0, //是否在屏幕左边
-                isRight = widthPercentInView === 0 && rect.top > 0 //是否在屏幕右边 
-            return {
-                isInView,
-                isTopIn,
-                isBottomIn,
-                isLeftIn,
-                isRightIn,
-                heightPercentInView,
-                widthPercentInView,
-                isAbove,
-                isBelow,
-                isLeft,
-                isRight,
-                rect
-            }
-        }
-
-        for (let id in items) {
-            let i = items[id],
-                {
+                let isInView = (isTopIn || isBottomIn) && (isRightIn || isLeftIn);
+                let isInViewOnePixMore = (isTopInOnePixMore || isBottomInOnePixMore) && (isRightInOnePixMore || isLeftInOnePixMore);
+                // 头部进入view viewportBottom - elementTop 
+                // 底部进入view elementBottom - viewportTop
+                let onScreenHeigh = isTopInOnePixMore || isBottomInOnePixMore ? ((isBottomInOnePixMore ? elementBottom : viewportBottom) - (isTopInOnePixMore ? elementTop : viewportTop)) : 0;
+                let onScreenWidth = isLeftInOnePixMore || isRightInOnePixMore ? ((isRightInOnePixMore ? elementRight : viewportRight) - (isLeftInOnePixMore ? elementLeft : viewportLeft)) : 0;
+                let heightPercentInView = parseFloat(onScreenHeigh / (rect.height || 1) || 0).toFixed(2),
+                    widthPercentInView = parseFloat(onScreenWidth / (rect.width || 1) || 0).toFixed(2),
+                    isAbove = heightPercentInView === 0 && rect.top <= 0, //是否在屏幕中顶部
+                    isBelow = heightPercentInView === 0 && rect.top > 0, //是否在屏幕底部
+                    isLeft = widthPercentInView === 0 && rect.left <= 0, //是否在屏幕左边
+                    isRight = widthPercentInView === 0 && rect.top > 0 //是否在屏幕右边 
+                let showResult = {
                     isInView,
+                    isInViewOnePixMore,
                     isTopIn,
+                    isTopInOnePixMore,
                     isBottomIn,
+                    isBottomInOnePixMore,
                     isLeftIn,
+                    isLeftInOnePixMore,
                     isRightIn,
+                    isRightInOnePixMore,
+                    onScreenHeigh,
+                    onScreenWidth,
                     heightPercentInView,
                     widthPercentInView,
                     isAbove,
@@ -140,97 +137,87 @@ function getPlugin() {
                     isLeft,
                     isRight,
                     rect
-                } = getInType(i),
-                inViewChange = !i.isInView && isInView,
-                outViewChange = i.isInView && !isInView
-
-            if (!i.isInView && !isInView) continue //当前没有，前一个状态也没有在屏幕内 直接返回
-            i.rect = rect;
-
-            let eventType = (inViewChange && EventTypes.Enter) || (outViewChange && EventTypes.Exit) || EventTypes.Progress;
-
-            if (typeof i.handler === 'function') {
-                i.handler({
-                    isInView,
-                    isTopIn,
-                    isBottomIn,
-                    isLeftIn,
-                    isRightIn,
-                    heightPercentInView,
-                    widthPercentInView,
-                    isAbove,
-                    isBelow,
-                    isLeft,
-                    isRight,
-                    rect,
-                    target: i,
-                    callbkParam:i.callbkParam
-                })
-            }
-
-            if (typeof i.onceenter === 'function' && eventType === EventTypes.Enter) {
-                i.onceenter({
-                    isInView,
-                    isTopIn,
-                    isBottomIn,
-                    isLeftIn,
-                    isRightIn,
-                    heightPercentInView,
-                    widthPercentInView,
-                    isAbove,
-                    isBelow,
-                    isLeft,
-                    isRight,
-                    rect,
-                    target: i,
-                    callbkParam:i.callbkParam
-                })
-                delete i.onceenter
-            }
-
-            i.isInView = isInView;
-        }
-
-        scrollValue = viewportTop;
-    }
-
-    Vue.directive('on-screen', {
-        unbind: function (element, bind) {
-            delete items[element.$scrollId]
-        },
-        inserted: function (element, bind) {
-            let id = element.$scrollId || ('scrollId-' + itemIndex++),
-                item = items[id] || {
-                    element: element,
-                    classes: {},
-                    percent: -1,
-                    rect: {}
                 }
-
-            //bind的value 支持 对象
-            let bValue = bind.value;
-            //console.log(bValue);
-            if (bind.modifiers && bind.modifiers.once) {
-                item.onceenter = bValue.callbk
-            } else {
-                item.handler = bValue.callbk
+                return showResult;
             }
-            //minShowHeight要考虑底部tab导航被遮挡的问题高度。
-            item.minShowHeight = bValue.minShowHeight || 0;
-            item.minShowWidth = bValue.minShowWidth || 0;
-            item.callbkParam = bValue.callbkParam || {}; //用于回调的入参
-            element.$scrollId = id
-            items[id] = item
-            scrollThrottledHandler()
-        }
-    })
-}
 
-return {
-    install: function (Vue, options) {
-        Vue.directive('on-screen', Vue.prototype.$isServer ? {} : createInstance(Vue, options || {}))
+            for (let id in items) {
+                let i = items[id],
+                    returnInType = getInType(i),
+                    inViewChange = !i.isInView && returnInType.isInView,
+                    outViewChange = i.isInView && !returnInType.isInView
+
+                //if (!i.isInView && !returnInType.isInView) continue //当前没有，前一个状态也没有在屏幕内 直接返回
+                i.rect = returnInType.rect;
+
+                let eventType = (inViewChange && EventTypes.Enter) || (outViewChange && EventTypes.Exit) || EventTypes.Progress;
+                let param = Object.assign(returnInType, {
+                    eventType,
+                    target: i,
+                    callbkParam: i.callbkParam || {}
+                });
+                if (typeof i.onceenter === 'function' && eventType === EventTypes.Enter) {
+                    i.onceenter(param)
+                    delete i.onceenter;
+                } else if (typeof i.onceexit === 'function' && eventType === EventTypes.Exit) {
+                    i.onceexit(param)
+                    delete i.onceexit;
+                } else if (typeof i.always === 'function') {
+                    i.always(param);
+                } else if (typeof i.handler === 'function' && returnInType.isInView) { //在屏幕内就调用
+                    i.handler(param)
+                }
+                i.isInView = returnInType.isInView;
+            }
+
+            scrollValue = viewportTop;
+        }
+
+        Vue.directive('on-screen', {
+            unbind: function (element, bind) {
+                delete items[element.$scrollId]
+            },
+            inserted: function (element, bind) {
+                let id = element.$scrollId || ('scrollId-' + itemIndex++),
+                    item = items[id] || {
+                        element: element,
+                        classes: {},
+                        percent: -1,
+                        rect: {}
+                    }
+
+                //bind的value 支持 对象
+                let bValue = bind.value;
+                if (bind.modifiers && (bind.modifiers.once || bind.modifiers.onceenter)) { //进入只执行一次
+                    item.onceenter = bValue.callbk
+                }
+                if (bind.modifiers && bind.modifiers.onceexit) { //退出调用一次
+                    item.onceexit = bValue.callbk
+                }
+                if (bind.modifiers && bind.modifiers.always) { //总是调用,不管在不在屏幕内
+                    item.always = bValue.callbk
+                }
+                if (!bind.modifiers.once && !bind.modifiers.onceenter && !bind.modifiers.onceexit && !bind.modifiers.always) {
+                    item.handler = bValue.callbk
+                }
+                //minShowHeight要考虑底部tab导航被遮挡的问题高度。
+                Object.assign(item, bValue);
+                // item.minShowHeight = bValue.minShowHeight || 0;
+                // item.minShowWidth = bValue.minShowWidth || 0;
+                // item.minShowWidth = bValue.minShowWidth || 0;
+                // item.callbkParam = bValue.callbkParam || {}; //用于回调的入参
+                element.$scrollId = id
+                items[id] = item
+                scrollThrottledHandler()
+            }
+        })
     }
-}
+
+    return {
+        install: function (Vue, options) {
+            Vue.directive('on-screen', Vue.prototype.$isServer ? {} : createInstance(Vue, options || {}))
+        }
+    }
 }
 
 if (typeof exports === 'object' && typeof module !== 'undefined') {
--- readme.md
+++ readme.md
@@ -7,24 +7,30 @@ npm i vue-on-screen
 ```
 
 ```
-//在vue的main.js文件中添加如下代码
+
 import vueOnScreen from 'vue-on-screen'
 Vue.use(vueOnScreen)
 
 ```
 
-## Add directive 'on-screen' to elements
+## Add directive 'v-on-screen' to elements
 
 ### Example
 
 ```
-<!--minShowHeight:展示最低多少的高度才算展示,100表示在屏幕内展示出100px的高度,才算该元素展示了默认为1 -->
-<!--minShowHeight:展示最低多少的宽度才算展示,100表示在屏幕内展示出100px的宽度,才算该元素展示了默认为1 -->
+<!--minShowHeight:展示最低多少的高度才算展示,100表示在屏幕内展示出100px的高度,才算该元素展示了, 默认为1 -->
+<!--isForceHeight:isForceHeight 强制是否强制使用传入的高度,一般在元素是动态高度的时候需要传入,默认为false，表示计算 minShowHeight = Math.min(元素高度,minShowHeight) -->
+<!--minShowWidth:展示最低多少的宽度才算展示,100表示在屏幕内展示出100px的宽度,才算该元素展示了 ,默认为1 -->
+<!--isForceWidth:isForceWidth 强制是否强制使用传入的宽度,一般在元素是动态款固定的时候需要传入,默认为false，表示计算 minShowWidth = Math.min(元素宽度,minShowWidth), -->
 <!--callbkParam:表示位置需要传给回调函数callbk的对象,和callbk配合使用,默认值{} -->
-<!--callbk:当页面触发scroll 和resize的时候回调函数，默认为空 -->
-<!--once，vue的modifiers,可以仅回调一次。和callbk配合使用>
-<div v-on-sreen.once=="{
+<!--callbk:当元素符合条件展示在页面内的时候回调函数，默认为空 -->
+<!--once，vue的modifiers,可以仅回调一次，这个和onceenter表现一致。为了向前兼容。和callbk配合使用>
+<!--onceenter，vue的modifiers,可以在该元素符合进入条件时回调一次。和callbk配合使用>
+<!--onceexit，vue的modifiers,可以在该元素符合退出条件时回调一次。和callbk配合使用>
+<!--always，vue的modifiers,页面滚动的时候总是执行回调。和callbk配合使用>
+<div v-on-screen.onceenter="{
     minShowHeight:100,
+    isForceHeight:true,
     minShowWidth:100,
     callbkParam:{
         index:2
@@ -37,11 +43,14 @@ Vue.use(vueOnScreen)
 
 ```
 funtion onScreenCallbk (e) {
+    console.log(e.eventType) // 'enter' 'exit' 'process' 表示当前元素在屏幕的位置
     console.log(e.isInView) // 是否在页面内展示了
     console.log(e.isTopIn）// 是否顶部在页面内
     console.log(e.isBottomIn) //是否底部在页面内
     console.log(e.isLeftIn) //是否左边缘在页面内
     console.log(e.isRightIn) //是否右边缘在页面内
+    console.log(e.onScreenHeigh) //展示出来的高度PX
+    console.log(e.onScreenWidth) //展示出来的宽度Px
     console.log(e.heightPercentInView) //展示出来的高度占元素总高度的百分比
     console.log(e.widthPercentInView) //展示出来的宽度占元素总宽度的百分比
     console.log(e.isAbove) //是否在屏幕的顶部
